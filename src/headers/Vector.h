1456c3842fc530cf602186226b7a241254ab87fe
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-01-17 19:41:57 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@93 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/headers/Vector.h
@@ -0,0 +1,25 @@
+/* 
+ * $Id$
+ */
+
+/********************** Vector.h ************************************
+ *                                                                  *
+ * *****************************************************************/
+
+
+#ifndef __VECTOR_H
+#define __VECTOR_H
+
+namespace numth
+{
+  template<class T>
+    class Vector
+    {
+      public:
+        Vector();
+        Vector( const Vector<T>& rhs);
+
+      private:
+        _vectorData 
+
+#endif

26ae6ddc6a537e643157d215b7598d4d73007d5b
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-01-17 19:42:35 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@94 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/tests/cppunit/aux.h
@@ -0,0 +1,16 @@
+#ifndef __AUX_H
+#define __AUX_H
+
+#include <cstdlib>
+#include <cassert>
+
+inline int brand(int a, int b){
+
+  assert(a < b);
+
+  int x = rand() % (b-a);
+  return x+a;
+}
+
+
+#endif

0ffaa5967a46259f4420ee3e460d3bb495b18576
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-01-23 20:23:45 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@119 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/testVelocidadRC4.cpp
+++ src/sources/tests/testVelocidadRC4.cpp
@@ -7,7 +7,11 @@ int main()
  NumThRC4Gen rc4;
 // congruentGen cong;
 
- rc4.leerBits(419430400);
+ rc4.leerBits(419430400); // 50 MB
+ rc4.leerBits(419430400); // 50 MB
+ rc4.leerBits(419430400); // 50 MB
+ rc4.leerBits(419430400); // 50 MB
+
 // cong.leerBits(419430400);
 
 }

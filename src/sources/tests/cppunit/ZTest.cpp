a37091e08384259ee14e78f3bba73cd9db1439ec
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-01-28 11:34:34 +0000

    more test cases
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@132 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/cppunit/ZTest.cpp
+++ src/sources/tests/cppunit/ZTest.cpp
@@ -43,15 +43,132 @@ void ZTest::testAddition(){
   CPPUNIT_ASSERT( uno + uno == dos );
   CPPUNIT_ASSERT( uno + cero == uno );
   CPPUNIT_ASSERT( cero + cero == cero );
+
+  Z res = z1 + z2;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " + ";
+  tmp += z2.toString();
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+
+
 }
 void ZTest::testSubstraction(){
   CPPUNIT_ASSERT( dos - uno == uno );
   CPPUNIT_ASSERT( cero - uno == -uno );
   CPPUNIT_ASSERT( cero - cero == cero );
+
+  Z res = z1 - z2;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " - ";
+  tmp += z2.toString();
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
 };
+void ZTest::testMultiplication(){
+  CPPUNIT_ASSERT( dos * uno == dos );
+  CPPUNIT_ASSERT( cero * uno == cero );
+  CPPUNIT_ASSERT( cero * cero == cero );
+
+  Z res = z1 * z2;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " * ";
+  tmp += z2.toString();
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+};
+void ZTest::testDivision(){
+  CPPUNIT_ASSERT( dos / uno == dos );
+  CPPUNIT_ASSERT( cero / uno == cero );
+
+  Z res = z1 / z2;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " / ";
+  tmp += z2.toString();
+  
+  GEN x = gp_read_str(const_cast<char*>(tmp.c_str()));
+  x = gtrunc(x);
+
+  string pariStr(GENtostr( x ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+};
+
+void ZTest::testModulus(){
+  CPPUNIT_ASSERT( dos % uno == cero );
+  CPPUNIT_ASSERT( uno % dos == uno );
+
+  Z res = z1 % z2;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " % ";
+  tmp += z2.toString();
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+};
+void ZTest::testExponentiation(){
+  CPPUNIT_ASSERT( (dos ^ uno) == dos );
+  CPPUNIT_ASSERT( (dos ^ cero) == uno );
+
+  z2  = funcs.getRandomRapido()->leerBits(brand(4,6));
+  Z res = (z1 ^ z2) ;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " ^ ";
+  tmp += z2.toString();
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+};
+
+void ZTest::testSquare(){
+  Z res = (cuadrado(z1)) ;
+   
+  string tmp;
+  tmp += z1.toString();
+  tmp += " ^ ";
+  tmp += "2";
+
+  string pariStr(GENtostr( gp_read_str((char*)tmp.c_str()) ));
+  string thisStr = res.toString();
+
+  CPPUNIT_ASSERT_EQUAL( pariStr, thisStr );
+};
+
+
 void ZTest::testDivideByZeroThrows(){
   uno / cero;
 }
+void ZTest::testModulusByZeroThrows(){
+  uno % cero;
+}
+
+
 
 void ZTest::testFactorial(){
   Cifra rnd = (funcs.getRandomRapido()->leerCifra()) % (1UL<<10);
--- src/sources/tests/cppunit/ZTest.h
+++ src/sources/tests/cppunit/ZTest.h
@@ -22,7 +22,12 @@ class ZTest : public CppUnit::TestFixture {
     CPPUNIT_TEST( testEquality );
     CPPUNIT_TEST( testAddition );
     CPPUNIT_TEST( testSubstraction );
+    CPPUNIT_TEST( testMultiplication );
+    CPPUNIT_TEST( testDivision );
+    CPPUNIT_TEST( testExponentiation);
+    CPPUNIT_TEST( testSquare );
     CPPUNIT_TEST_EXCEPTION( testDivideByZeroThrows, Errores::DivisionPorCero); 
+    CPPUNIT_TEST_EXCEPTION( testModulusByZeroThrows, Errores::DivisionPorCero); 
 
     CPPUNIT_TEST( testFactorial );
   CPPUNIT_TEST_SUITE_END();
@@ -38,7 +43,13 @@ class ZTest : public CppUnit::TestFixture {
     void testEquality();
     void testAddition();
     void testSubstraction();
+    void testMultiplication();
+    void testDivision();
+    void testModulus();
+    void testExponentiation();
+    void testSquare();
     void testDivideByZeroThrows();
+    void testModulusByZeroThrows();
 
     void testFactorial();
   private:

165d7499a1da455e8fdadae03fb3c214dda531fb
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-02-08 20:27:28 +0000

    - Function repository change towards the generic getFunc/setFunc mechanism
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@160 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/headers/AbstractMethod.h
@@ -0,0 +1,12 @@
+#ifndef __ABSTRACTMETHOD_H
+#define __ABSTRACTMETHOD_H
+
+
+class AbstractMethod{
+  public:
+    virtual ~AbstractMethod(){}// = 0; //force the class to be abstract
+};
+
+//AbstractMethod::~AbstractMethod(){}
+
+#endif

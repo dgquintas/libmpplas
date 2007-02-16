58fb328d6b17d54ef84fb56887f8a8443893fffd
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-02-16 20:28:10 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@185 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/tests/testMatrix.cpp
@@ -0,0 +1,25 @@
+#include "Matrix.h"
+#include <iostream>
+
+
+using namespace numth;
+using namespace std;
+
+
+int main(){
+
+  Matrix<int> mat(3,3);
+
+  cin >> mat;
+
+
+  cout << mat;
+
+  cout << endl;
+  mat.setNumberColumns(2);
+
+  cout << mat;
+
+
+  return 0;
+}

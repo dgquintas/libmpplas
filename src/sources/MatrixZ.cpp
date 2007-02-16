475ff50ac33a5da3872db6861420e0c6dc266e43
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-02-16 20:31:35 +0000

    initial version
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@190 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/MatrixZ.cpp
@@ -0,0 +1,27 @@
+#include "MatrixZ.h"
+
+namespace numth
+{
+  MatrixZ::MatrixZ()
+    : Matrix<Z>()
+  {}
+
+  MatrixZ::MatrixZ(const MatrixZ& m)
+    : Matrix<Z>(m)
+  {}
+
+  MatrixZ::MatrixZ(const Matrix<Z>& m)
+    : Matrix<Z>(m)
+  {}
+
+
+  MatrixZ::MatrixZ(const size_t nAndm)
+    : Matrix<Z>(nAndm) {}
+  MatrixZ::MatrixZ(const size_t n, const size_t m)
+    : Matrix<Z>(n,m) {}
+
+
+
+}
+  
+

ef0dbe979adc75bbad652283e79e8b9abe005781
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-02-28 20:34:39 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@210 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/testRSA.cpp
+++ src/sources/tests/testRSA.cpp
@@ -20,7 +20,7 @@ int main()
   //esto no es estrictamente necesario: para acceder de forma resumida
   //al generador del numeros aleatorios en cuestion
   RandomRapido* genRandom; funcs->getFunc(genRandom);
-  genRandom->ponerSemilla("34");
+  genRandom->ponerSemilla(Z("34"));
   //idem para el generador de primos
   GenPrimos* genPrimos; funcs->getFunc(genPrimos);
   genPrimos->setRandomSeed((Cifra)123);

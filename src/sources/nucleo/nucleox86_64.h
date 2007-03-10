f4b6e148986ee8aa4807fd2f7b2d25da5d77b759
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-03-10 11:49:30 +0000

    Vectorial CPU and basic cpu methods made static, ie, functions inside a namespace. Still work in progress on the kernels. (basic cpu is not a template anymore)
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libnumth/trunk@215 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/nucleo/nucleox86_64.h
+++ src/sources/nucleo/nucleox86_64.h
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <unistd.h>
-#include <stdlib.h>
 /* 
  * $Id$
  */
@@ -11,20 +8,12 @@
    *            IMPLEMENTACI�N PARA ARQUITECTURA X86_64              *
    *                                                                 *
    ******************************************************************/ 
-  /** Constructor por defecto de la Cpu B�sica para x86_64 */
-  template<>
-    inline vCPUBasica<Arch::x86_64>::vCPUBasica()
-      : overflow(0), resto(0)
-    {
-    }
- 
 
   /** Suma b�sica de dos Cifras para x86_64.
    *
    * @sa Add(Cifra arg1, Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Add(Cifra arg1, Cifra arg2)
+    inline Cifra Add(Cifra arg1, Cifra arg2, Cifra& overflow)
     {
       __asm__ ("xorq %[_of], %[_of];"
                "addq %[_arg2], %[_arg1]; "
@@ -41,8 +30,7 @@
    *
    * @sa Addx(Cifra arg1, Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Addx(Cifra arg1, Cifra arg2) 
+    inline Cifra Addx(Cifra arg1, Cifra arg2, Cifra& overflow) 
     { 
       __asm__ ("btrq $0, %[_of];"
                "adcq %[_arg2], %[_arg1];" 
@@ -60,8 +48,7 @@
    *
    * @sa Sub(Cifra arg1, Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Sub(Cifra arg1, Cifra arg2) 
+    inline Cifra Sub(Cifra arg1, Cifra arg2, Cifra& overflow) 
     { 
       __asm__  ("xorq %[_of], %[_of];"
                "subq %[_arg2], %[_arg1];"
@@ -77,8 +64,7 @@
    *
    * @sa Subx(Cifra arg1, Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Subx(Cifra arg1, Cifra arg2) 
+    inline Cifra Subx(Cifra arg1, Cifra arg2, Cifra& overflow) 
     { 
       __asm__  ("btrq $0, %[_of];" /* pone CF al valor del bit 0 de overflow */
                "sbbq %[_arg2], %[_arg1];"
@@ -94,8 +80,7 @@
    *
    * @sa Mul(Cifra arg1,Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Mul(Cifra arg1,Cifra arg2) 
+    inline Cifra Mul(Cifra arg1,Cifra arg2, Cifra& resto) 
     { 
       __asm__ (" mulq %[_arg2]" 
           : "=a" (arg1), "=d" (resto) 
@@ -109,8 +94,7 @@
    *
    * @sa Addmul(Cifra arg1,Cifra arg2) 
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Addmul(Cifra arg1,Cifra arg2) 
+    inline Cifra Addmul(Cifra arg1,Cifra arg2, Cifra& resto) 
     { 
       __asm__ (" mulq %[_arg2];"
                "addq %[_restoViejo],%[_ret];" 
@@ -126,8 +110,7 @@
    *
    * @sa Div(Cifra arg1, Cifra arg2)
    */
-   template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Div(Cifra arg1, Cifra arg2) 
+    inline Cifra Div(Cifra arg1, Cifra arg2, Cifra& resto) 
     { 
       __asm__ (" divq %[_arg2]" 
           : "=a" (arg1), "=d" (resto) 
@@ -142,8 +125,7 @@
     *
     *  @sa Shiftl(Cifra arg1,Cifra arg2)
     */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Shiftl(Cifra arg1,Cifra arg2) 
+    inline Cifra Shiftl(Cifra arg1,Cifra arg2, Cifra& resto) 
     { 
       __asm__ ("xorq %[_resto], %[_resto];"
                "shldq %%cl, %[_arg1], %[_resto]; "
@@ -159,8 +141,7 @@
    *
    * @sa Shiftlr(Cifra arg1,Cifra arg2)
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Shiftlr(Cifra arg1,Cifra arg2) 
+    inline Cifra Shiftlr(Cifra arg1,Cifra arg2, Cifra& resto) 
     { 
       __asm__ ("xorq %[_resto], %[_resto];"
                "shrdq %%cl, %[_arg1], %[_resto];"
@@ -176,8 +157,7 @@
    *
    * @sa Bfffo(Cifra arg1) 
    */
-  template<>
-    inline Cifra vCPUBasica<Arch::x86_64>::Bfffo(Cifra arg1) 
+    inline Cifra Bfffo(Cifra arg1) 
     {
       Cifra ret = (Constantes::BITS_EN_CIFRA-1), temp; 
 
@@ -193,48 +173,5 @@
       return ret; 
     }
 
-  /** Funci�n de perfilado en x86_64 (inv�lido).
-   *
-   * Invicar esta funci�n produce que se lance una excepci�n de tipo 
-   * Errores::ArchNoProfiling
-   */
-  template<>
-    inline void vCPUBasica<Arch::x86_64>::reiniciarContadores(void)
-    {
-      throw Errores::ArchNoProfiling();
-    }
-
-   /** Funci�n de perfilado en x86_64 (inv�lido).
-   *
-   * Invicar esta funci�n produce que se lance una excepci�n de tipo 
-   * Errores::ArchNoProfiling
-   */
- template<>
-    inline void vCPUBasica<Arch::x86_64>::inicioProf(void)
-    {
-      throw Errores::ArchNoProfiling();
-    }
-
-   /** Funci�n de perfilado en x86_64 (inv�lido).
-   *
-   * Invicar esta funci�n produce que se lance una excepci�n de tipo 
-   * Errores::ArchNoProfiling
-   */
- template<>
-    inline void vCPUBasica<Arch::x86_64>::finProf(void)
-    {
-      throw Errores::ArchNoProfiling();
-    }
-
-   /** Funci�n de perfilado en x86_64 (inv�lido).
-   *
-   * Invicar esta funci�n produce que se lance una excepci�n de tipo 
-   * Errores::ArchNoProfiling
-   */
- template<>
-    inline ResultadosProf vCPUBasica<Arch::x86_64>::obtenerPerfil(void)
-    {
-      throw Errores::ArchNoProfiling();
-    }
 
 

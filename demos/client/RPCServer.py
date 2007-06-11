657824d47203fedd497c2950043ad74fac0ec2fa
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-06-11 16:02:54 +0000

    - Use of MultiCall RPC  (batch)
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@306 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- demos/client/RPCServer.py
+++ demos/client/RPCServer.py
@@ -1,10 +1,49 @@
 from singletonmixin import Singleton
-from xmlrpclib import ServerProxy
+from xmlrpclib import ServerProxy,MultiCall
 
-class RPCServer(Singleton, ServerProxy):
+
+class RPCServer(Singleton):
 
   def __init__(self, serverURL, **rpcServerExtra):
-    ServerProxy.__init__(self, serverURL, **rpcServerExtra) 
+    self.__interactiveRPC = self._RPCServerProxy(serverURL, **rpcServerExtra)
+    self.__batchRPC = self._RPCMultiCall(self.__interactiveRPC);
+
+    self.__useBatch = False
+
+  def getServer(self):
+    if self.isBatch():
+      return self.getBatchServer();
+    else:
+      return self.getInteractiveServer()
+
+  def getInteractiveServer(self):
+    return self.__interactiveRPC;
+
+  def getBatchServer(self):
+    return self.__batchRPC;
+
+  def setBatch(self, useBatch):
+    if useBatch:
+      self.__batchRPC = self._RPCMultiCall(self.__interactiveRPC);
+    self.__useBatch = useBatch;
+
+  def isBatch(self):
+    return self.__useBatch
+
+  def resetBatch(self):
+    self.__batchRPC = self._RPCMultiCall(self.__interactiveRPC)
+
+
+  #############################################
+  # Private inner classes
+  ##############################
+  class _RPCServerProxy(ServerProxy):
+    def __init__(self, serverURL, **rpcServerExtra):
+      ServerProxy.__init__(self, serverURL, **rpcServerExtra) 
+
+  class _RPCMultiCall(MultiCall):
+    def __init__(self, serverProxy):
+      MultiCall.__init__(self, serverProxy)
 
 
   

d5ea9ffa7be211d35b0def0d661cae4d77cc8c46
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-06-11 16:05:45 +0000

    - Use of MultiCall RPC  (batch)
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@309 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- demos/client/plotEx.py
+++ demos/client/plotEx.py
@@ -1,15 +1,30 @@
 from matplotlib.pylab import *
 from Client import *
 
-def piEuler(x):
-	numPrimes = 0
-	for i in xrange(x):
-		if isPrime(i):
-			numPrimes += 1
-	return numPrimes
+def piEuler(last, first=3, initialCount=1):
+  assert(last >= first)
+  numPrimes = initialCount
+  beginBatch()
+  if not (first & 0x1): #even
+    first += 1
+  for i in xrange(first,last,2):
+    isPrime(i)
+  res = runBatch()
+  numPrimes += res.count(True)
+  return numPrimes
 
-t1 = arange(2,1000,50)
+t1 = arange(100,1001,100)
+piEulerResList = []
+piEulerResList.append( piEuler(t1[0]) )
+for i in xrange(1,len(t1)):
+  piEulerResList.append( piEuler(first = t1[i-1]+1, last = t1[i], initialCount = piEulerResList[-1]) )
+
+
 xlabel(r"$n$")
 ylabel(r"$\pi(n)$")
-plot(t1, map(lambda x: piEuler(x), t1), 'ro-', t1, t1/log(t1), 'bx-')
+plot(t1, piEulerResList, 'ro-', label=r"True pi(n)")
+plot(t1, t1/log(t1), 'bx-', label=r"n/log(n) approx.")
+legend()
+
+
 

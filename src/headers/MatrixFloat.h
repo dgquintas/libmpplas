b970d7f24ca9fb8c8abdc80bfdcea5da1ea52f38
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-06-23 21:32:05 +0000

    WIP
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@330 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/headers/MatrixFloat.h
@@ -0,0 +1,60 @@
+/*
+ * $Id$
+ */
+
+#ifndef __MATRIXFLOAT_H
+#define __MATRIXFLOAT_H
+
+#include "Matrix.h"
+#include "BasicTypedefs.h"
+
+namespace mpplas 
+{
+  class MatrixFloat : public Matrix< __m128 >
+  {
+    public:
+      MatrixFloat();
+      MatrixFloat(const MatrixFloat& m);
+      MatrixFloat(const Matrix<__m128>& m);
+
+      MatrixFloat(const size_t nAndm);
+      MatrixFloat(const size_t n, const size_t m);
+
+      MatrixFloat(const std::string& str);
+
+      MatrixFloat& operator+=(const MatrixFloat&);
+      MatrixFloat& operator+=(const float&);
+
+      MatrixFloat& operator-=(const MatrixFloat&);
+      MatrixFloat& operator-=(const float&);
+
+      MatrixFloat& operator*=(const MatrixFloat&);
+      MatrixFloat& operator*=(const float&);
+
+      MatrixFloat& operator/=(const MatrixFloat&);
+      MatrixFloat& operator/=(const float&);
+
+      MatrixFloat& operator^=(const float&);
+
+      MatrixFloat& diagonalize();
+      MatrixFloat& invert();
+
+      float getDeterminant();
+
+
+    private:
+      friend std::istream& operator>>(std::istream& is, MatrixFloat& m ) ;
+  
+
+
+  };
+
+  MatrixFloat operator-(MatrixFloat m); /**< Unary negation, sign inversion */
+
+  std::istream& operator>>(std::istream& is, MatrixFloat& m);
+
+}
+
+
+#endif
+

9377ad2c112b9eac93759ce718032756bced7c71
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-07-03 19:33:35 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@345 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/Utils.cpp
@@ -0,0 +1,16 @@
+#include "Utils.h"
+
+namespace mpplas{ 
+  namespace Utils {
+    std::string strip(const std::string& str, 
+        const std::string SEPSET){
+
+      const std::string::size_type first = str.find_first_not_of(SEPSET);
+      return ( first==std::string::npos ) ? 
+        std::string()  
+        :
+        str.substr(first, str.find_last_not_of(SEPSET)-first+1);
+    }
+  }
+}
+

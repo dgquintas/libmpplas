7e3bc7fd067337f3ed8b2fb8887b015836699b50
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-09-04 17:14:17 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@367 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/tests/units/PrimesTest.h
@@ -0,0 +1,37 @@
+/*
+ * $Id: RandomTest.h 254 2007-04-11 18:37:15Z dgquintas $
+ */
+
+#ifndef __PRIMESTEST_H
+#define __PRIMESTEST_H
+
+#include <string>
+
+#include "qtunit/TestCase.h"
+#include "Primos.h"
+
+
+using namespace mpplas;
+
+namespace com_uwyn_qtunit{
+ 
+  class PrimesTest: public TestCase {
+
+    public:
+    PrimesTest();
+
+    void setUp();
+    void tearDown();
+
+    protected:
+    /* The actual tests */
+    void testRabinMiller();
+    void testMersenneLucasLehmer();
+    void testGenerator();
+
+    private:
+      RabinMiller _rm;
+      GenPrimos _primeGen;
+  };
+}
+#endif

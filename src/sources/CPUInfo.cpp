ac53cb8bfcacfba3dbeb82d38d7fd7bef4bd39f4
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-09-04 17:08:53 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@365 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

---
+++ src/sources/CPUInfo.cpp
@@ -0,0 +1,37 @@
+#include "CPUInfo.h"
+#include "BasicTypedefs.h"
+#include "CompilationConfig.h"
+
+namespace mpplas{
+
+  int CPUInfo::getCacheL1Size() const {
+    return _l1Size;
+  }
+  int CPUInfo::getCacheL2Size() const {
+    return _l2Size;
+  }
+  int CPUInfo::getCacheL3Size() const {
+    return _l3Size;
+  }
+
+  std::string CPUInfo::getModelName() const {
+    return _modelName;
+  }
+
+  std::string CPUInfo::getArchName() const {
+    return std::string(CompilationConfig::TARGET_ARCH);
+  }
+
+  std::vector<std::string> CPUInfo::getSIMDCapabilities() const {
+    return _simdCap;
+  }
+
+  int CPUInfo::getDigitBitWidth() const {
+    return sizeof(Digit)*8;
+  }
+
+  int CPUInfo::getAvailableCPUs() const {
+    return omp_get_num_procs();
+  }
+
+}

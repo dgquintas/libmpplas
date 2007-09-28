5a38e735661f97aa7a207b2626716c599f647fe3
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-09-28 17:53:58 +0000

    - fixed SERIOUS bug: inheriting from STL's vector. Using composition instead now (private inheritance)
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@387 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/headers/MiVec.h
+++ src/headers/MiVec.h
@@ -11,8 +11,8 @@
 namespace mpplas{
 
   /** Vector propio de la librería. */
-  template<class T>
-    class MiVec : public std::vector<T>
+  template<typename T, typename Alloc = std::allocator<T> >
+    class MiVec : private std::vector<T, Alloc>
     {
       public:
         MiVec(); /**< Constructor por defecto. */
@@ -20,13 +20,30 @@ namespace mpplas{
         MiVec(size_t tam, const T& ini);/**< Construir vector reservando @a tam posiciones 
                                              y rellenándolas con el valor @a ini */
 
-        MiVec(const MiVec<T>&); /**< Constructor de copia. */
+        MiVec(const std::vector<T, Alloc>&); /**< Constructor de copia. */
 
-        MiVec& operator=(const MiVec<T>& otro)
-        { 
-          std::vector<T>::operator=(otro);
-          return *this;
-        }
+        MiVec& operator=(const MiVec<T, Alloc>& rhs);
+
+        using std::vector<T, Alloc>::size;
+        using std::vector<T, Alloc>::clear;
+        using std::vector<T, Alloc>::push_back;
+        using std::vector<T, Alloc>::insert;
+        using std::vector<T, Alloc>::front;
+        using std::vector<T, Alloc>::back;
+        using std::vector<T, Alloc>::begin;
+        using std::vector<T, Alloc>::end;
+        using std::vector<T, Alloc>::pop_back;
+        using std::vector<T, Alloc>::erase;
+        using std::vector<T, Alloc>::iterator;
+        using std::vector<T, Alloc>::const_iterator;
+        using std::vector<T, Alloc>::resize;
+        using std::vector<T, Alloc>::reverse_iterator;
+        using std::vector<T, Alloc>::rbegin;
+        using std::vector<T, Alloc>::rend;
+        using std::vector<T, Alloc>::reference;
+        using std::vector<T, Alloc>::const_reference;
+        using std::vector<T, Alloc>::reserve;
+//        using std::vector<T, Alloc>::
 
 
 #ifndef RELEASE
@@ -60,25 +77,31 @@ namespace mpplas{
     };
 
 
-  template<class T>
-    MiVec<T>::MiVec()
-    : std::vector<T>()
+  template<typename T, typename Alloc>
+    MiVec<T, Alloc>::MiVec()
+    : std::vector<T, Alloc>()
     { }
 
-  template<class T>
-    MiVec<T>::MiVec(const MiVec<T>& otro)
-    : std::vector<T>(otro)
+  template<typename T, typename Alloc>
+    MiVec<T, Alloc>::MiVec(const std::vector<T, Alloc>& src)
+    : std::vector<T, Alloc>(src)
     { }
  
-  template<class T>
-    MiVec<T>::MiVec(size_t tam)
-    : std::vector<T>(tam)
+  template<typename T, typename Alloc>
+    MiVec<T, Alloc>::MiVec(size_t tam)
+    : std::vector<T, Alloc>(tam)
     { }
 
-  template<class T>
-    MiVec<T>::MiVec(size_t tam, const T& inicio)
-    : std::vector<T>(tam, inicio)
+  template<typename T, typename Alloc>
+    MiVec<T, Alloc>::MiVec(size_t tam, const T& inicio)
+    : std::vector<T, Alloc>(tam, inicio)
     { }
+  
+  template<typename T, typename Alloc>
+  MiVec<T, Alloc>& MiVec<T, Alloc>::operator=(const MiVec<T, Alloc>& rhs) { 
+    std::vector<T,Alloc>::operator=(rhs);
+    return *this;
+  }
 
   
 

825662c425c4865278756d0641089188d20a3289
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-10-10 15:02:13 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@464 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/testRandom.cpp
+++ src/sources/tests/testRandom.cpp
@@ -8,7 +8,8 @@ int main()
 {
   NumThRC4Gen rnd;
 
-  cout << rnd.leerBits(128 * 1024 * 8) << endl; //16KB
+  cout << rnd.getInteger(128 * 1024 * 8) << endl; //16KB
+  cout << rnd.getInteger(0) << endl; //16KB
 
   
   return 0;

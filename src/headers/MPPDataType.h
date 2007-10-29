2388bf81745a5de10182e924a1e43c810f895ada
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-10-29 21:40:52 +0000

    added doc
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@483 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/headers/MPPDataType.h
+++ src/headers/MPPDataType.h
@@ -6,12 +6,18 @@
 #define __MPPDATATYPE_H
 
 #include <string>
+#include <sstream>
 
 namespace mpplas{
 
   class MPPDataType {
     public:
+      /** Get a text representation of the data.
+       *
+       * @return A std::string representing the standard textual representation of the data.
+       */
       virtual std::string toString() const = 0;
+
   };
 
 }

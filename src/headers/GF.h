e3442ee8c44ec4df93e523296d7b73961d061610
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-11-01 03:13:42 +0000

    finished implementation of generation/tests of irreducible/primitive polynomials
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@532 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/headers/GF.h
+++ src/headers/GF.h
@@ -11,9 +11,9 @@
 
 namespace mpplas{
 
-  class GF : public Z_px /*, public Field< GF >*/ {
+  class GF : public Z_px , public Field< GF > {
     public:
-//      GF(const Z& p, const Digit n = 1);
+      GF(const Z& p, const int n = 1);
       GF(const Z& p, const Z_px& fx);
       GF(const Z_px& poly, const Z_px& fx);
 
@@ -34,21 +34,51 @@ namespace mpplas{
 
       GF& operator^=(const Z& exp);
 
-      static bool _isIrreducible(const Z_px& fx, const Z& p);
-      static Z_px _genIrreducible(const int degree, const Z& p);
+      static bool isIrreducible(const Z_px& fx, const Z& p);
+      static Z_px generateIrreducible(const int degree, const Z& p);
+      static bool isPrimitive(const Z_px& fx, const MiVec<Z>& factors, const Z& p);
+      static Z_px generatePrimitive(const int degree, const MiVec<Z>& factors, const Z& p);
+
+
+
+
+      /* Ring, Group and Field methods */
+      static const GF ZERO;
+      static const GF ONE;
+
+      static const bool addCommutative;
+      static const bool groupCyclic;
+
+      static const bool unitaryRing;
+      static const bool multCommutative;
+      static const bool multAssociative;
+      static const bool divisionRing;
+
+      GF getMultInverse() const {
+        return this->getInverse();
+      }
+      static const GF& getMultIdentity() {
+        return GF::ONE ;
+      };
+      static const GF& getAddIdentity() {
+        return GF::ZERO;
+      };
+      GF getAddInverse() const{
+        return GF(Z_px::getAddInverse(), _fx);
+      };
+      static const GF& getGroupGenerator() {
+        return GF::ONE;
+      }
+
 
     protected:
       Z_px _fx;
 
       Z _p;
-      Digit _n;
+      int _n;
 
-//      static Z_px _genPrimitivePolynomial(const int degree);
 
   }; /* class GF */
-
-
-
 
   inline Z GF::getOrder() const{
     return (_p ^ _n);
@@ -57,6 +87,8 @@ namespace mpplas{
     return _fx;
   }
 
+
+  GF operator^(GF lhs, const Z& rhs);
 
 
 
--- src/sources/GF.cpp
+++ src/sources/GF.cpp
@@ -10,9 +10,23 @@
 
 namespace mpplas {
 
-//  GF::GF(const Z& p, const Digit n)
-//    : Z_px(p), _p(p), _n(n)
-//  {}
+
+  const GF GF::ZERO( GF(Z_px(Z_p((Digit)0, (Digit)0, false, false), 0, 0), Z_px::ZERO ) );
+  const GF GF::ONE ( GF(Z_px(Z_p((Digit)1, (Digit)0, false, false), 0, 0), Z_px::ZERO ) );
+
+  const bool GF::groupCyclic(true);
+  const bool GF::addCommutative(true); 
+
+  const bool GF::multAssociative(true);
+  const bool GF::multCommutative(true);
+  const bool GF::unitaryRing(true);
+  const bool GF::divisionRing(true);
+
+
+  GF::GF(const Z& p, const int n)
+    : Z_px(p), _p(p), _n(n),_fx(generateIrreducible(n,p)){
+      this->operator%=(_fx);
+  }
 
   GF::GF(const Z& p, const Z_px& fx)
     : Z_px(p), _n( fx.getDegree() ), _p(p), _fx(fx) {
@@ -23,21 +37,31 @@ namespace mpplas {
     : Z_px(poly), _n( poly.getDegree() ), _fx(fx), _p( poly.getCharacteristic() )
   {
     assert( fx.getCharacteristic() == poly.getCharacteristic() ); //FIXME: raise exception
-    this->operator%=(fx);
+    if( !fx.isZero() ){
+      this->operator%=(fx);
+    }
   }
 
   GF& GF::operator*=(const GF& rhs){
+    if( this->_fx.isZero() ){
+      this->_fx = rhs._fx;
+    }
     ((Z_px*)this)->operator*=(rhs);
     this->operator%=(_fx);
 
     return *this;
   }
-  GF& GF::square(){
-    ((Z_px*)this)->square();
-    this->operator%=(_fx);
+  GF& GF::square(){ 
+    if( !this->_fx.isZero() ){
+      ((Z_px*)this)->square();
+      this->operator%=(_fx);
+    }
     return *this;
   }
   GF& GF::operator/=(const GF& rhs){
+    if( this->_fx.isZero() ){
+      this->_fx = rhs._fx;
+    }
     GF inv(rhs);
     inv.invert();
     this->operator*=(inv);
@@ -57,9 +81,11 @@ namespace mpplas {
   }
 
   GF& GF::operator^=(const Z& exp){
-    Exponentiation< GF > *potMod; 
-    MethodsFactory::getReference().getFunc(potMod);
-    potMod->exponentiation(this,exp);
+    if( !this->_fx.isZero() ){
+      Exponentiation< GF > *potMod; 
+      MethodsFactory::getReference().getFunc(potMod);
+      potMod->exponentiation(this,exp);
+    }
     return *this;
   }
 
@@ -71,7 +97,8 @@ namespace mpplas {
     return *this;
   }
 
-  bool GF::_isIrreducible(const Z_px& fx, const Z& p ){
+  bool GF::isIrreducible(const Z_px& fx, const Z& p ){
+    //TODO: fx has to be monic
     assert( fx.getCharacteristic() == p );
     const int m = fx.getDegree();
     const Z_px x(Z_px(1, 1, p));
@@ -88,7 +115,7 @@ namespace mpplas {
     return true; //irreducible
 
   }
-  Z_px GF::_genIrreducible(const int m, const Z& p){
+  Z_px GF::generateIrreducible(const int m, const Z& p){
     RandomFast *rnd; MethodsFactory::getReference().getFunc(rnd);
     
     Z_px fx(Z::ONE, m, p);
@@ -101,14 +128,56 @@ namespace mpplas {
         fx[i] = rnd->getIntegerBounded(p);
       }
     }
-    while( !_isIrreducible(fx, p) );
+    while( !isIrreducible(fx, p) );
 
     return fx;
     
   }
-//  Z_px GF::_genPrimitivePolynomial(const int degree){
-//  }
 
+
+  bool GF::isPrimitive(const Z_px& fx, const MiVec<Z>& factors, const Z& p){
+    //TODO: fx has to be monic
+    if( !isIrreducible(fx, p) ){ // primitive => irreducible
+      return false;
+    } 
+    Exponentiation< GF > *potMod; 
+    MethodsFactory::getReference().getFunc(potMod);
+
+    const int m( fx.getDegree() );
+    const GF lx(Z_px(1,1, p), fx);
+    GF ltmp(lx);
+    const Z pM_1( ((p ^ m) -1) );
+    for(int i = 0 ; i < factors.size(); i++){
+      potMod->exponentiation(&ltmp, pM_1 / factors[i]);
+      if( ltmp.isOne() ){
+        return false;
+      }
+      ltmp = lx;
+    }
+    return true;
+  }
+
+
+
+  Z_px GF::generatePrimitive(const int degree, const MiVec<Z>& factors, const Z& p){
+    Z_px fx(p);
+    do{
+      fx = generateIrreducible( degree , p);
+    } while( !isPrimitive(fx,factors,p) );
+
+    return fx;
+  }
+
+
+
+
+
+
+
+  GF operator^(GF lhs, const Z& rhs){
+    lhs ^= rhs;
+    return lhs;
+  }
 
 
 }

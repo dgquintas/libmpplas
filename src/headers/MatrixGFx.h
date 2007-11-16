e3c979669ceff9bcfee778ccaf5f7a8ce677afd4
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-11-16 11:07:04 +0000

    Matrices on GFx need to be associated with a GF generator
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@607 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/headers/MatrixGFx.h
+++ src/headers/MatrixGFx.h
@@ -7,20 +7,23 @@
 
 #include "Matrix.h"
 #include "GFx.h"
+#include "GF.h"
 
 namespace mpplas {
-  class MatrixGFx : public Matrix<GFx>
-  {
+  class MatrixGFx : public Matrix<GFx> {
     public:
-      MatrixGFx();
-      MatrixGFx(const int nAndm);
-      MatrixGFx(const int n, const int m);
-      MatrixGFx(const Dimensions& dims);
+      MatrixGFx(const GF& generator);
+      MatrixGFx(const int nAndm, const GF& generator);
+      MatrixGFx(const int n, const int m, const GF& generator);
+      MatrixGFx(const Dimensions& dims, const GF& generator);
       MatrixGFx(const MatrixGFx& m);
       MatrixGFx(const Matrix<GFx>& m);
-      MatrixGFx(const std::string& str);
+      MatrixGFx(const std::string& str, const GF& generator);
 
       MatrixGFx& operator=(const MatrixGFx& src);
+
+    protected:
+      GF _gfGenerator;
 
   };
 }
--- src/sources/MatrixGFx.cpp
+++ src/sources/MatrixGFx.cpp
@@ -1,34 +1,58 @@
 #include "MatrixGFx.h"
+#include "MatrixZ.h"
+
 
 namespace mpplas{
 
-  MatrixGFx::MatrixGFx()
-    : Matrix<GFx>()  {}
+  MatrixGFx::MatrixGFx(const GF& generator)
+    : Matrix<GFx>(), _gfGenerator(generator)  {
+    }
 
-  MatrixGFx::MatrixGFx(const int nAndm)
-    : Matrix<GFx>(nAndm)  {}
+  MatrixGFx::MatrixGFx(const int nAndm, const GF& generator)
+    : Matrix<GFx>(nAndm) , _gfGenerator(generator) {
+      for(int i = 0; i < this->_data.size(); i++){
+        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
+      }
+    }
 
-  MatrixGFx::MatrixGFx(const int n, const int m)
-    : Matrix<GFx>(n,m)  {}
+  MatrixGFx::MatrixGFx(const int n, const int m, const GF& generator)
+    : Matrix<GFx>(n,m), _gfGenerator(generator)  {
+      for(int i = 0; i < this->_data.size(); i++){
+        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
+      }
+    }
 
-  MatrixGFx::MatrixGFx(const Dimensions& dims)
-    : Matrix<GFx>(dims)  {}
+  MatrixGFx::MatrixGFx(const Dimensions& dims, const GF& generator)
+    : Matrix<GFx>(dims), _gfGenerator(generator)  {
+          for(int i = 0; i < this->_data.size(); i++){
+        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
+      }
+
+    }
   
   MatrixGFx::MatrixGFx(const MatrixGFx& m)
-    : Matrix<GFx>(m)  {}
+    : Matrix<GFx>(m), _gfGenerator(m._gfGenerator)  {}
   
   MatrixGFx::MatrixGFx(const Matrix<GFx>& m)
-    : Matrix<GFx>(m)  {}
+    : Matrix<GFx>(m), _gfGenerator(m(0,0).getGenerator())  {
+          for(int i = 0; i < this->_data.size(); i++){
+        this->_data[i] = _gfGenerator.getElement( this->operator[](i).getIntegerValue() );
+      }
+    }
   
-  MatrixGFx::MatrixGFx(const std::string& str)
-    : Matrix<GFx>(str)  {}
-  
+  MatrixGFx::MatrixGFx(const std::string& str, const GF& generator)
+    : Matrix<GFx>() , _gfGenerator(generator) {
+      MatrixZ tmp(str);
+      for(int i = 0; i < tmp.getSize(); i++){
+        this->_data.push_back(_gfGenerator.getElement( tmp[i] ));
+      }
+      this->_dims = tmp.getDimensions();
+    }
 
   MatrixGFx& MatrixGFx::operator=(const MatrixGFx& src){
     Matrix<GFx>::operator=(src);
     return *this;
   }
-
 
 
 }

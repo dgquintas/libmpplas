155d3c4e20da59533fccdf8232df9427040af9b9
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-11-16 11:04:18 +0000

    updated with runInParallel
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@600 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- demos/client/RSA.py
+++ demos/client/RSA.py
@@ -1,23 +1,22 @@
 from Client import *
 def getKeyPair(keySize):
-	" Returns the pair (public, private) keys"
-	p = getPrime(keySize)
-	q = getPrime(keySize)
-	n = p*q
-	phi = (p-1)*(q-1)
-	while True:
-		e = randomZLessThan(n)
-		if gcd(e,phi) == 1:
-			break
-	d = modInverse(e,phi)
-	return ((e,n) , (d,n))
+  " Returns the pair (public, private) keys"
+  (p,q) = runInParallel(["result=getPrime(%d)" % keySize ]*2,2, "Generating prime pair (p,q)")
+  n = p*q
+  phi = (p-1)*(q-1)
+  while True:
+    e = getRandomZLessThan(n)
+    if str(gcd(e,phi)) == '1':
+      break
+  d = modInverse(e,phi)
+  return ((e,n) , (d,n))
 
 def cypher(key, msg):
-	e = key[0]
-	n = key[1]
-	return modExp(msg,e,n)
+  e = key[0]
+  n = key[1]
+  return modExp(msg,e,n)
 
 def decypher(key, cyphertext):
-	d = key[0]
-	n = key[1]
-	return modExp(cyphertext,d,n)
+  d = key[0]
+  n = key[1]
+  return modExp(cyphertext,d,n)

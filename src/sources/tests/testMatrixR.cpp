5bdaacb71c9b4454ffb9d2be6b0ca57f5a5bf313
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-11-26 13:51:06 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@619 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/testMatrixR.cpp
+++ src/sources/tests/testMatrixR.cpp
@@ -1,5 +1,9 @@
 #include "MatrixR.h"
 #include <iostream>
+#include "Profiling.h"
+#include "Random.h"
+#include "MethodsFactory.h"
+#include "omp_mock.h"
 
 
 using namespace mpplas;
@@ -8,33 +12,74 @@ using namespace std;
 
 int main(){
 
-  MatrixR mat("[1324.1 21 3.35236; 4.111 5.222 0.6; -75345.3253232522342 82325232235626234.1 9.122]");
-  
 
+  Profiling& prof( Profiling::getReference() );
+  R::precisionSalida(6);
+
+  //MatrixR mat("[1.31 12.2 43.35 331.1; 4 1.4 35.55 66.61; 7 3.74 18.18 19.9 ; 1 2 3 4]");
+  RandomFast* rnd;
+  MethodsFactory::getReference().getFunc(rnd);
+  rnd->setSeed(Z::ZERO);
+  MatrixR mat(30,30);
+
+  for(int i=0; i < mat.getRows(); i++){
+    for(int j =0 ; j < mat.getColumns(); j ++){
+      mat(i,j) = R(rnd->getInteger(32)); 
+      mat(i,j) /= R(rnd->getInteger(23));
+    }
+  }
+  std::cout << "LOL" << std::endl;
+//  MatrixR b("[4.5; 6.7; 8.9; 7.8]");
+//  MatrixR mat3( "[1324.1 21 3.35236; 4.111 5.222 0.6; -75345.3253232522342 82325232235626234.1 9.122]");
+//  MatrixR foo;
+//
+//  cout << (mat * mat3) << endl;
+//
+//  foo = mat;
+//  cout << foo << endl;
+//
+
+
+  //  std::cout << b << std::endl;
+//  prof.reset();
+//  std::cout << MatrixHelpers::solve(mat,b) << std::endl;
+
+//  cout <<  prof.getResults() << endl;
+  prof.reset();
+  prof.startClock();
+  MatrixHelpers::invert(mat);
+  cout <<  prof.getResults() << endl;
+  cout <<  prof.stopClock()  << endl;
+  //
+  //
+  //
+  //  cout << mat << endl;
+//  MatrixHelpers::makeCroutsCombinedMatrix(mat);
 //  cout << mat << endl;
 //
-//  MatrixR mat2(mat);
 //
-//  cout << mat2 << endl;
 //
-//  mat2.transpose();
-//  mat2.transpose();
-//
-//  cout << (mat == mat2) << endl;
 
-  MatrixR mat3( mat.toString() );
-//  MatrixR mat3( "[1324.1 21 3.35236; 4.111 5.222 0.6; -75345.3253232522342 82325232235626234.1 9.122]");
+//  for(int i = 0; i < 3 ; i++){
+//    for(int j=0; j < i; j++){
+//      l(i,j) = M(i,j);
+//    }
+//    l(i,i) = R::ONE;
+//  }
+//  for(int i = 0; i < 3 ; i++){
+//    for(int j=i; j < 3; j++){
+//      u(i,j) = M(i,j);
+//    }
+//  }
+////
+//  std::cout << l << std::endl;
+//  std::cout << u << std::endl;
+//  std::cout << ((l*u)-Morig) << std::endl;
 
-//"[1324.1 21 3.35236; 4.111 5.222 0.6; -75345.3253232522342 82325232235626234.1 9.122]"
+//  MatrixHelpers::forwardSubstitution(l, b);
+//  MatrixHelpers::backwardSubstitution(r, b);
 
-//  cout << mat.toString() << endl;
 
-//  cout << mat3 << endl;
-
-  cout << (mat == mat3) << endl;
-
-  cout << mat.toString() << endl;
-  cout << mat3.toString() << endl;
 
 
   return 0;

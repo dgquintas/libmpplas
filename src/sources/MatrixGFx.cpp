b6e08905e7f69abad25dcdc081c7985bf85126e5
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2007-12-11 15:47:10 +0000

    optimizations
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@676 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/MatrixGFx.cpp
+++ src/sources/MatrixGFx.cpp
@@ -9,36 +9,29 @@ namespace mpplas{
     }
 
   MatrixGFx::MatrixGFx(const int nAndm, const GF& generator)
-    : Matrix<GFx>(nAndm) , _gfGenerator(generator) {
-      for(int i = 0; i < this->_data.size(); i++){
-        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
-      }
+    : Matrix<GFx>() , _gfGenerator(generator) {
+      this->_dims.setBoth(nAndm, nAndm);
+      this->_data.resize(nAndm*nAndm, _gfGenerator.getElement());
     }
 
   MatrixGFx::MatrixGFx(const int n, const int m, const GF& generator)
-    : Matrix<GFx>(n,m), _gfGenerator(generator)  {
-      for(int i = 0; i < this->_data.size(); i++){
-        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
-      }
+    : Matrix<GFx>(), _gfGenerator(generator)  { 
+      this->_dims.setBoth(n, m);
+      this->_data.resize(n*m, _gfGenerator.getElement());
     }
 
   MatrixGFx::MatrixGFx(const Dimensions& dims, const GF& generator)
-    : Matrix<GFx>(dims), _gfGenerator(generator)  {
-          for(int i = 0; i < this->_data.size(); i++){
-        this->_data[i] = _gfGenerator.getElement(Z::ZERO);
-      }
+    : Matrix<GFx>(), _gfGenerator(generator)  {
 
+      this->_dims = dims;
+      this->_data.resize(dims.getProduct(), _gfGenerator.getElement());
     }
   
   MatrixGFx::MatrixGFx(const MatrixGFx& m)
     : Matrix<GFx>(m), _gfGenerator(m._gfGenerator)  {}
   
   MatrixGFx::MatrixGFx(const Matrix<GFx>& m)
-    : Matrix<GFx>(m), _gfGenerator(m(0,0).getGenerator())  {
-          for(int i = 0; i < this->_data.size(); i++){
-        this->_data[i] = _gfGenerator.getElement( this->operator[](i).getIntegerValue() );
-      }
-    }
+    : Matrix<GFx>(m), _gfGenerator(m(0,0).getGenerator())  { }
   
   MatrixGFx::MatrixGFx(const std::string& str, const GF& generator)
     : Matrix<GFx>() , _gfGenerator(generator) {

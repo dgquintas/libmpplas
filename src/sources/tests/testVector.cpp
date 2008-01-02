993b1af22cfa7e502dc37e16f21967713005cac3
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2008-01-02 00:46:28 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@708 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/testVector.cpp
+++ src/sources/tests/testVector.cpp
@@ -1,23 +1,42 @@
-#include "Vector.h"
+#include "VectorFloat.h"
+#include "VectorInt.h"
+#include "VectorDouble.h"
 #include <iostream>
+#include <cstdlib>
+#include "Profiling.h"
 
 
 using namespace mpplas;
 using namespace std;
 
 
-int main(){
+int main(int argc, char** argv){
 
-  Vector<float> vec("[1324.32 21.32 3.1 4435.2 5.325 6.3 -75423.1 -328.1 -9]");
+  Profiling& prof( Profiling::getReference() );
+  const int SIZE = atoi(argv[1]);
+  VectorFloat vec(SIZE);
+  VectorFloat vec2(SIZE);
+  float tmp[4]   __attribute__((aligned(16)));
+  float tmp2[4]  __attribute__((aligned(16)));
 
-  cout << vec << endl;
-  cout << vec.toString() << endl;
-  cout << vec(1) << " " << vec(3) << endl;
+  for( int i = 0; i < SIZE/4 ; i++){
+    for(int j = 0 ; j < 4; j++){
+      tmp[j] = float(rand())/rand();
+      tmp2[j] = float(rand())/rand();
+    }
+    vec(i).pack(tmp);
+    vec2(i).pack(tmp2);
+  }
 
-  vec.transpose();
+//  cout << vec - vec2  << endl;
+//  cout << vec * vec2  << endl;
+//  cout << vec / vec2  << endl;
 
-  cout << vec << endl;
-  cout << vec.toString() << endl;
+  prof.startClock();
+  for( int i = 0 ; i < 10 ; i++ ){
+    vec *= vec2;
+  }
+  cout <<  prof.stopClock() << endl ;
 
 
 

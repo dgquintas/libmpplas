838784c4dd59232e10636c6491c603a8308e72db
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2008-01-02 00:46:20 +0000

    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@707 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/kernel/kernelSIMD.h
+++ src/sources/kernel/kernelSIMD.h
@@ -218,9 +218,28 @@ namespace mpplas{
 
 namespace mpplas{
 
-  std::ostream& operator<<(std::ostream& out, const SIMDDigit<float4xSIMD_t>& v);
-  std::ostream& operator<<(std::ostream& out, const SIMDDigit<double2xSIMD_t>& v);
-  std::ostream& operator<<(std::ostream& out, const SIMDDigit<int8xSIMD_t>& v);
+  inline std::ostream& operator<<(std::ostream& out, const SIMDDigit<float4xSIMD_t>& v){
+    float x[4] __attribute__((aligned(16)));
+    SIMDCPU::Unpack(x,v);
+    out << x[0] << " " << x[1] << " " << x[2] << " " << x[3];
+    return out;
+  }
+  inline std::ostream& operator<<(std::ostream& out, const SIMDDigit<double2xSIMD_t>& v){
+    double x[2] __attribute__((aligned(16)));
+    SIMDCPU::Unpack(x,v);
+    out << x[0] << " " << x[1] ;
+    return out;
+  }
+
+  inline std::ostream& operator<<(std::ostream& out, const SIMDDigit<int8xSIMD_t>& v){
+    int16_t x[8] __attribute__((aligned(16)));
+    SIMDCPU::Unpack(x,v);
+    out << x[0] << " " << x[1] << " " << x[2] << " " << x[3] << " " 
+      << x[4] << " " << x[5] << " " << x[6] << " " << x[7] ;
+    return out;
+  }
+
+
 
   template<typename T>
   SIMDDigit<T> operator+(SIMDDigit<T> lhs, const SIMDDigit<T>& rhs){

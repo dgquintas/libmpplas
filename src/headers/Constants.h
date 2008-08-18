727e13ed860f01e190f88b1f7df05a41b9a5cfad
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2008-08-18 19:51:08 +0000

    primes table changed from int to Digit
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@740 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/headers/Constants.h
+++ src/headers/Constants.h
@@ -199,7 +199,7 @@ namespace mpplas{
       false, false, false, false, true, false, false, true};
 
       /** Primes up to 2000. */
-    static const int TABLA_PRIMOS_2000[] = {
+    static const Digit TABLA_PRIMOS_2000[] = {
       2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
       73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
       157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,

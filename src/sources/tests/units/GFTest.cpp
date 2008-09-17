feb0be90142461a930cfa5383f4c55e2d4685650
dgquintas <dgquintas@ea26bf2b-611e-0410-8ca6-ad9ecb09ac58>
2008-09-17 15:23:24 +0000

    WIP. need to implement another test for forces throwing an exception when trying to invert a zero
    git-svn-id: https://libmpplas.svn.sourceforge.net/svnroot/libmpplas/libmpplas/trunk@748 ea26bf2b-611e-0410-8ca6-ad9ecb09ac58

--- src/sources/tests/units/GFTest.cpp
+++ src/sources/tests/units/GFTest.cpp
@@ -50,19 +50,19 @@ void GFTest::tearDown(){
 
 void GFTest::testGeneratorFromPrimitivePoly(){
   const Z p(primes->getPrime(brand(1,4)));
-  const int n(brand(1,4));
- 
+  const int n(brand(1,6));
+
   GF gfPrim(p,n,true);
 
   std::set<GFx> elems;
   const GFx x( gfPrim.getElement("[(1,1)]") );
-  for( int i = 0; i < gfPrim.getOrder(); i++){
+  for( SignedDigit i = 0; i < gfPrim.getOrder(); i++){
     GFx xi(x);
     xi ^= i;
     elems.insert(xi);
   }
 
-  qassertTrue((gfPrim.getOrder()-1) == elems.size());
+  qassertTrue((gfPrim.getOrder()-Z::ONE) == elems.size());
 
 }
 void GFTest::testGetElement(){}
@@ -72,7 +72,7 @@ void GFTest::testAddition(){
   GFx one = gfIrred->getElement(Z::ONE);
   GFx accum(one);
 
-  for(Z i = 0 ; i < charact-1 ; i++){
+  for(Z i = Z::ZERO ; i < charact-Z::ONE ; i++){
     accum += one;
   }
 
@@ -80,36 +80,43 @@ void GFTest::testAddition(){
 }
 void GFTest::testSubstraction(){}
 void GFTest::testMultiplication(){
-  const GFx lhs(gfIrred->getElement( Z(rnd->getDigit()) ));
-  const GFx rhs(gfIrred->getElement( Z(rnd->getDigit()) ));
-  GFx accum = GFx(rhs);
-
-  const GFx expected( lhs*rhs );
-
-  const Digit times( lhs.getIntegerValue()[0] );
-  std::cout << times << std::endl;
-  for(Digit i = 1; i < times ; i++){
-    accum += rhs;
-    if( (i % 10000) == 0 ){
-      std::cout << i << std::endl;
-    }
-  }
-
-  std::cout << accum << "\n" << expected << std::endl;
-
-  qassertTrue( accum == expected );
+//  const GFx lhs(gfIrred->getElement( Z(rnd->getDigit()) ));
+//  const GFx rhs(gfIrred->getElement( Z(rnd->getDigit()) ));
+//  GFx accum = GFx(rhs);
+//
+//  const GFx expected( lhs*rhs );
+//
+//  const Digit times( lhs.getIntegerValue()[0] );
+//  std::cout << times << std::endl;
+//  for(Digit i = 1; i < times ; i++){
+//    accum += rhs;
+//    if( (i % 10000) == 0 ){
+//      std::cout << i << std::endl;
+//    }
+//  }
+//
+//  std::cout << accum << "\n" << expected << std::endl;
+//
+//  qassertTrue( accum == expected );
 
 }
 void GFTest::testSquare(){}
 void GFTest::testDivision(){}
 
 void GFTest::testInversion(){
-  const GFx e(gfIrred->getElement( Z(rnd->getDigit()) ));
+  const Z srcInt( rnd->getDigit() );
+  const GFx e(gfIrred->getElement( srcInt ));
   const GFx eInv( e.getInverse() );
 
   const GFx shouldBeOne ( e*eInv );
 
-  qassertTrue( shouldBeOne == gfIrred->getElement( Z::ONE ) );
+  const bool res = shouldBeOne == gfIrred->getElement( Z::ONE );
+  if( !res ){
+    std::cout << gfIrred->toHRString() << std::endl;
+    std::cout << srcInt << " " << e << " " << eInv << std::endl;
+    std::cout << shouldBeOne << std::endl;
+  }
+  qassertTrue( res );
 
 }
 void GFTest::testExponentiation(){}
